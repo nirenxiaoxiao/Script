616e292e6be2863d72fa81c905cb65158689865a
hourh <525848327@qq.com>
2018-11-07 20:12:52 +0800

    first commit:

---
+++ getexcel.bat
@@ -0,0 +1,30 @@
+;; 百度搜索 关于公布新增固定式 site:www.bjjtgl.gov.cn ，这里只是2016年以来的。有的日期是前一天。
+
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20181011.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180913.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180821.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180723.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180620.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180531.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180510.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180418.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180306.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20180130.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20171025.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20170904.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20170705.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20170616.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20170303.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20161124.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20161101.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20160902.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20160711.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20160621.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20160129.xls{,x}
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshebei20150626.xls{,x}
+
+
+curl -O http://www.bjjtgl.gov.cn/Portals/0/%E5%9B%BA%E5%AE%9A%E5%BC%8F%E8%AE%BE%E5%A4%87%E6%B7%BB%E5%8A%A0%E7%82%B9%E4%BD%8D%E8%A1%A82014.12.30.xls
+curl -O http://www.bjjtgl.gov.cn/Portals/0/%E5%9B%BA%E5%AE%9A%E5%BC%8F%E8%AE%BE%E5%A4%87%E6%B7%BB%E5%8A%A0%E7%82%B9%E4%BD%8D%E8%A1%A820140813.xls
+curl -O http://www.bjjtgl.gov.cn/Portals/0/gudingshi20150925.xls
+curl -O http://www.bjjtgl.gov.cn/uploadfile/20110714008.xls
